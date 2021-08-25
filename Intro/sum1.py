46a21989dbc5e5f745aba7ae2c8d58cb9c43383c
Gleb Nemetsky <glebdvp@gmail.com>
2021-08-25 20:33:35 +0600

    Добавлена вводная глава

---
+++ Intro/sum1.py
@@ -0,0 +1,19 @@
+# Программа sum1.py из примера по вводу/выводу стр 50
+print("Type integers, each followed by Enter; or just Enter to finish")
+
+total = 0
+count = 0
+while True:
+    line = input("integer: ")
+    if line:
+        try:
+            number = int(line)
+        except ValueError as err:
+            print(err)
+            continue
+        total += number
+        count += 1
+    else:
+        break
+if count:
+    print("count =", count, "total =", total, "mean =", total / count)
